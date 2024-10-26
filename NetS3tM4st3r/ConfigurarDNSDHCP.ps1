694fbc51859e7880379973905fe5d3eb6df57c76
Nahuel Espinoza <123512832+espinozan@users.noreply.github.com>
2024-10-26 00:02:25 -0300

    Delete NetS3tM4st3r directory

--- NetS3tM4st3r/ConfigurarDNSDHCP.ps1
+++
@@ -1,8 +0,0 @@
-# Obtiene el adaptador de red activo
-$adapter = Get-NetAdapter | Where-Object { $_.Status -eq 'Up' }
-if ($adapter) {
-    Set-DnsClientServerAddress -InterfaceAlias $adapter.Name -ResetServerAddresses
-    Write-Output "DNS configurado en DHCP para adaptador: $($adapter.Name)"
-} else {
-    Write-Output "Error: No se encontró un adaptador de red activo."
-}
